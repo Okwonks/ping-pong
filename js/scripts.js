af4036bcf1d53b22bc2b22efbf4e5f8557e08e6c
Okwonks <albertoketch@gmail.com>
2017-06-09 16:00:48 +0300

    adding code to the js for the ping pong function to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,40 @@
 //business logic
+var userInput;
+var userNumber;
+
 var numberArr = [];
-for (var index = 1; index <= input; index++){
-  numberArr.push(index)
-}
 
-numberArr.map(function(num) {
-  if (num % 3 && 5 === 0) {
-    num = "ping pong";      //returns ping pong for multiples of 3
-  } else if (num % 5 === 0){
-    num = "pong";           //returns pong for multiples of 5
-  } else if (num % 3 === 0) {
-    num = "ping";          //returns ping for multiples of 3
+console.log();
+var pingPong = function() {
+for (var index = 1; index <= userNumber; index++){
+  numberArr.push(index);
+
+  numberArr.map(function(userInput) {
+
+  if (userInput % 3 && 5 === 0) {
+    userInput = "ping pong";      //returns ping pong for multiples of 3
+  } else if (userInput % 5 === 0){
+    userInput = "pong";           //returns pong for multiples of 5
+  } else if (userInput % 3 === 0) {
+    userInput = "ping";          //returns ping for multiples of 3
   } else {
-    return index;          //returns the number as it is
+    // return index;          //returns the number as it is
   }
-
-})
+  return userInput;
+});
+return;
+};
+};
 
 //user interface logic
 $(document).ready(function() {
   $("form#pong-number").submit(function(event) {
     event.preventDefault();
-    var input = parseInt($("input#ping-pong"));
-    var result = numberArr(num);
-  })
-  $("#result").fadeIn(3000).fadeOut(15000);
-})
+    userInput = parseInt($("input#ping-pong"));
+    var result = pingPong(userInput);
+
+
+
+    $("#result").fadeIn(3000).fadeOut(15000);
+  });
+});
