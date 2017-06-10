37daae2adb48af230348119314fc04344dd18ccc
Okwonks <albertoketch@gmail.com>
2017-06-10 19:15:35 +0300

    making some changes to the css. Adding some colour and some text to the README.md file. Also some comments in the scripts.js file.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 >This project is a web that allows users to input a number and a a chain of numbers will be shown leading up to the number they have chosen. There is an exception, at any multiple of 3 the number changes to 'ping', at multiples of 5 the number changes to 'pong' and at multiples of 15 the number changes to 'pingpong'.
 
 ## Project Specs:
->On entering a number users should be able to:
+On entering a number users should be able to:
 1. Read any number divisible by 3 as 'ping'
 ⋅⋅* **example input**: 9
 ⋅⋅* **example output**: ping
@@ -18,3 +18,9 @@
 3. Read any number divisible by 3 as 'ping'
 ⋅⋅* **example input**: 10
 ⋅⋅* **example output**: pong
+
+[Click here](https://okwonks.github.io/ping-pong/) to see the project.
+
+
+
+&copy Albert Oketch | WillisCode
--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,12 @@ var userNumber;
 // for (var i = 0; i < 10; i++) {
 //   array[i]
 // }
-var numberArr = [];
+var numberArr = []; //make an array for the loop to store its values
 
 var pingPong = function(userInput) {
 for (var index = 1; index <= userInput; index++){
-
+//the for loop is run with regards to user input
+//the if statement tells the program when to replace numbers with the desired output
   if ((index % 3 === 0) && (index % 5 === 0)) {
     numberArr.push("pingpong");      //returns ping pong for multiples of 3
   } else if (index % 5 === 0){
@@ -23,13 +24,14 @@ return numberArr;
 
 //user interface logic
 $(document).ready(function() {
+  alert("Welcome to Ping-Pong!!")
   $("form#pong-number").submit(function(event) {
     event.preventDefault();
     var userInput = parseInt($("input#ping-pong").val());
     var results = pingPong(userInput);
 
     $("#results").text("");
-    $("#results").append("<ul>");
+    $("#results").append("<ul>"); //this is used to append the results from the loop and the for function
     results.forEach(function(result) {
       $("#results").append('<li>' + result + '</li>');
     });
